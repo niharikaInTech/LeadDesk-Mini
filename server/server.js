2db735cc65e3a8959a70a5c598181620968d059a
Niharika Chinnupati <niharikachinnupati@gmail.com>
2026-07-26 01:31:13 +0530

    Update CORS for new frontend URL

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://lead-desk-mini-xi.vercel.app"
+        "https://leaddesk-mini-backend-hmnq.onrender.com"
     ]
 }));
 app.use(express.json());
