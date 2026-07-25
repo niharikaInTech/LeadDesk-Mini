d9f25e77935f20090097a0ee1119da1b3e8a1a57
Niharika Chinnupati <niharikachinnupati@gmail.com>
2026-07-26 01:24:12 +0530

    Fix Lead model import case

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
-const Lead = require("./models/lead");
+const Lead = require("./models/Lead");
 
 const app = express();
 const port = process.env.PORT || 5000;
